2edf99df8a50878748e216952cea5e28b67fd11b
Robin Hes <ninjatuna@outlook.com>
2013-09-26 12:24:42 +0200

    verslag af behalve conclusie en lelijk auteurvermelding

---
+++ docs/opdracht 3/pwm_gen/resource/parameter_def_pkg.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+
+PACKAGE parameter_def IS
+constant D : INTEGER;
+constant BITSIZE : INTEGER;
+END parameter_def;
+
+PACKAGE BODY parameter_def IS
+constant D : INTEGER := 15;
+constant BITSIZE : INTEGER := 4;
+END parameter_def;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
