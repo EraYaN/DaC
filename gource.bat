524f080f586af5de8ff950bc400fd21bbb245a8d
Erwin de Haan <git@erayan.eu>
2013-10-03 10:40:49 +0200

    Added package for eps inclusion to preamble.

--- gource.bat
+++ gource.bat
@@ -1,4 +1,4 @@
 @echo off
-title Gource MiDeRP
+title Gource DaC
 "../gource/gource.exe" --fullscreen -1920x1080 --stop-at-end --file-idle-time 6000 --multi-sampling --key --max-files 0 --seconds-per-day 6
 pause
