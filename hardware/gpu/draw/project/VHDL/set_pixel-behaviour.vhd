6c137b0c0fdd3cfe282fd3504fe4d9d1f0b4cf08
Jorden Kerkhof <jor-den@hotmail.com>
2013-11-19 13:18:15 +0100

    set_pixel werkt

--- hardware/gpu/draw/project/VHDL/set_pixel-behaviour.vhd
+++ hardware/gpu/draw/project/VHDL/set_pixel-behaviour.vhd
@@ -2,28 +2,24 @@ library IEEE;
 use IEEE.std_logic_1164.ALL;
 
 architecture behaviour of set_pixel is
-signal w: std_logic;
+signal w : std_logic := '0';
 
 begin
 address <= std_logic_vector(Y & X & ASB);
 data <= color;
 
 
-
+ 
 process(clk, reset)
-begin
-	if(rising_edge(clk)) then
-		if(reset = '0') then
-			if(w = '0') then 
-		`	 w <= '1';
-			elsif( w ='1') then
-			 w <= '0';
 
-	
-			 
-			end if;
-			
-			
+begin
+	if (rising_edge(clk)) then
+		if(reset = '0') then
+			if( w ='0') then
+				w <= '1';
+			elsif ( w = '1') then
+				w <= '0';
+			end if; 
 		elsif(reset = '1') then
 			address <= (others => '0');
 			data <= (others => '0');
@@ -36,5 +32,8 @@ end process;
 		
 
 end behaviour;
+
+
+
 
 
