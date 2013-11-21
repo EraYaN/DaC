0018e911a2da9b52876218f330f81216a974d15e
Robin Hes <ninjatuna@outlook.com>
2013-11-21 09:44:20 +0100

    decoder synthetiseert

--- hardware/gpu/decoder/decoder_behaviour.vhd
+++ hardware/gpu/decoder/decoder_behaviour.vhd
@@ -14,9 +14,9 @@ architecture behaviour of decoder is
 
 	--signal sendready, reg_value, reg_id, reg_set : std_logic;
 	--signal data_out : std_logic_vector(InstrPacketSize-1 downto 0);
-	signal draw_x0, draw_x1 : std_logic_vector(SizeX-1 downto 0);
-	signal draw_y0, draw_y1 : std_logic_vector(SizeY-1 downto 0);
-	signal draw_color : std_logic_vector(SizeColor-1 downto 0);
+	signal draw_x0, next_draw_x0, draw_x1, next_draw_x1 : std_logic_vector(SizeX-1 downto 0);
+	signal draw_y0, next_draw_y0, draw_y1, next_draw_y1 : std_logic_vector(SizeY-1 downto 0);
+	signal draw_color, next_draw_color : std_logic_vector(SizeColor-1 downto 0);
 	--signal draw_enabled : std_logic_vector(NumDrawModules-1 downto 0);
 	
 begin
@@ -41,6 +41,12 @@ begin
 				instr_state <= initial;
 				data_in <= (others => '0');
 				instruction <= (others => '0');
+				draw_color <= (others => '0');
+				draw_x0 <= (others => '0');
+				draw_y0 <= (others => '0');
+				draw_x1 <= (others => '0');
+				draw_y1 <= (others => '0');
+				
 			else
 				if data = '1' then
 					packet_num <= next_packet_num;
@@ -48,6 +54,11 @@ begin
 					data_in <= di;
 					instruction <= next_instruction;
 				end if;
+				draw_color <= next_draw_color;
+				draw_x0 <= next_draw_x0;
+				draw_y0 <= next_draw_y0;
+				draw_x1 <= next_draw_x1;
+				draw_y1 <= next_draw_y1;
 			end if;
 		end if;
 	end process;
@@ -61,16 +72,26 @@ begin
 			--reset
 			next_instr_state <= decoding;
 			next_packet_num <= (others => '0');
-			next_instruction <= (others => '0');
+			next_instruction <= (others => '0');			
 			col <= (others => '0');
 			x0 <= (others => '0');
 			y0 <= (others => '0');
 			x1 <= (others => '0');
-			y1 <= (others => '0');				
+			y1 <= (others => '0');
+			next_draw_color <= (others => '0');
+			next_draw_x0 <= (others => '0');
+			next_draw_y0 <= (others => '0');
+			next_draw_x1 <= (others => '0');
+			next_draw_y1 <= (others => '0');
 		else
 			--defaults
 			done := '0';
-			instr := instruction;	
+			instr := instruction;
+			next_draw_color <= draw_color;
+			next_draw_x0 <= draw_x0;
+			next_draw_y0 <= draw_y0;
+			next_draw_x1 <= draw_x1;
+			next_draw_y1 <= draw_y1;
 			--logic depending on current packet in stream
 			case to_integer(packet_num) is
 				when 0 =>
@@ -81,27 +102,27 @@ begin
 						done := '1';
 					end if;
 					--pass through color
-					col <= data_in(SizeColor-1 downto 0);
+					next_draw_color <= data_in(SizeColor-1 downto 0);
 				when 1 => 
 					--pass through x0 coord
-					x0 <= data_in;
+					next_draw_x0 <= data_in;
 				when 2 => 
 					--pass through y0 coord
-					y0 <= data_in(SizeY-1 downto 0);
+					next_draw_y0 <= data_in(SizeY-1 downto 0);
 					---start loading next instruction next cycle if instruction is "pixel"
 					if instr = "0010" then
 						done := '1';
 					end if;
 				when 3 =>
 					--pass through x1 coord
-					x1 <= data_in;
+					next_draw_x1 <= data_in;
 					--start loading next instruction next cycle if instruction is "circle" or "fcircle"
 					if (instr = "0110" or instr = "0111") then
 						done := '1';
 					end if;
 				when 4 =>
 					--pass through y1 coord
-					y1 <= data_in(SizeY-1 downto 0);
+					next_draw_y1 <= data_in(SizeY-1 downto 0);
 					--start loading next instruction next cycle if instruction is "square", "fsquare" or "line"
 					if (instr = "0011" or instr = "0100" or instr = "0101") then
 						done := '1';
@@ -117,6 +138,12 @@ begin
 			end if;
 			next_instr_state <= decoding;
 			next_instruction <= instr;
+
+			col <= next_draw_color;
+			x0 <= next_draw_x0;
+			y0 <= next_draw_y0;
+			x1 <= next_draw_x1;
+			y1 <= next_draw_y1;
 		end if;
 	end process;
 
@@ -130,7 +157,15 @@ begin
 	--			y0 <= (others => '0');
 	--			x1 <= (others => '0');
 	--			y1 <= (others => '0');
-	--		end if;	
+	--		else
+	--			draw_color <= (others => '0');
+	--			x0 <= (others => '0');
+	--			y0 <= (others => '0');
+	--			x1 <= (others => '0');
+	--			y1 <= (others => '0');
+	--		end if;
 	--	end if;
 	--end process;
 end behaviour;
+
+
