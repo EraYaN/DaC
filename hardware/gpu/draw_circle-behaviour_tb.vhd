c4efd62740d2cd2976ee9129c54a5998e5efef41
Tu Hoang <tu1994@hotmail.com>
2013-11-25 12:31:05 +0100

    More revisions draw_circle + part of tb

--- hardware/gpu/draw_circle-behaviour_tb.vhd
+++ hardware/gpu/draw_circle-behaviour_tb.vhd
@@ -1,8 +1,11 @@
 library IEEE;
 use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
 use work.parameter_def.ALL;
 
-entity draw_circle is
+architecture behavioural of draw_circle_tb is
+
+component draw_circle is
    port(clk           :in    std_logic;
         reset         :in    std_logic;
         enable        :in    std_logic;
@@ -17,6 +20,26 @@ entity draw_circle is
         ramdata       :out   std_logic_vector(SizeRAMData-1 downto 0);
         draw_write    :out   std_logic;
         draw_can_access:in    std_logic);
-end draw_circle;
+end component draw_circle;
+
+signal clk,reset,enable,asb,done,draw_write,draw_can_acces :std_logic;
+signal x,w:std_logic_vector(SizeX-1 downto 0);
+signal y:std_logic_vector(SizeY-1 downto 0);
+signal colour:std_logic_vector(SizeColour-1 downto 0);
+signal ramaddr:std_logic_vector(SizeRAMAddr-1 downto 0);
+signal ramdata:std_logic_vector(SizeRAMData-1 downto 0);
+
+
+begin 
+	lbl1: draw_circle port map (clk, reset, enable, x, y ,w, colour, asb, done, ramaddr,ramdata,draw_write,draw_can_access);
+
+	clk <=	'1' after 0 ns,
+		'0' after 10 ns when clk/= '0' else '1' after 10 ns;
+	reset <= '1' after 0 ns,
+		 '0' after 10 ns;
+	enable <= '0' after 0 ns,
+		  '1' after 10 ns;
+	x <= "00000000"
+	y <= "
 
 
---
+++ hardware/gpu/draw_circle_tb.vhd
@@ -0,0 +1,6 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use work.parameter_def.ALL;
+
+entity draw_circle_tb is
+end entity;
