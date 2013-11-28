ce0b78346d743156edda46d81a476e7c2b3c2086
Kees Hogenhout <keeshogenhout@hotmail.com>
2013-11-28 11:36:53 +0100

    draw_pixel

--- hardware/gpu/draw_pixel-behaviour.vhd
+++ hardware/gpu/draw_pixel-behaviour.vhd
@@ -4,36 +4,44 @@ use work.parameter_def.ALL;
 
 architecture behaviour of draw_pixel is
 signal busy : std_logic; -- used as a "state"
+signal ram_addr_tmp: std_logic_vector(SizeRAMAddr-1 downto 0);
+signal ramdata_tmp: std_logic_vector(SizeRAMData-1 downto 0);
 begin
-	process (clk)
+dp1:	process (clk)
 	begin
 		if rising_edge(clk) then
 			
-			if reset = '0' and enable = '1' and draw_can_access = '1' then -- RAM is free to access
 				if busy = '0' then --we are not busy							
-					ramaddr <= std_logic_vector(NOT asb & y & x); --combineer signalen
-					ramdata <= color; -- zet data op de bus
+					ram_addr_tmp <= std_logic_vector(NOT asb & y & x); --combineer signalen
+					ramdata_tmp <= color; -- zet data op de bus
 					draw_write <= '1'; -- vertel de controller dat je wil schrijven
 					done <= '0'; -- not done yet
 					busy <= '1'; -- we are now busy
 				else --we are busy							
 					done <= '1'; -- we are done now
 					busy <= '0'; -- we are not busy anymore
-					ramaddr <= (others => 'Z');
-					ramdata <= (others => 'Z');
 					draw_write <= '0';
 				end if;
-			else	
-				ramaddr <= (others => 'Z');
-				ramdata <= (others => 'Z');
+							
+				
 				draw_write <= '0';			
 				done <= '0';
 				busy <= '0';
-			end if;
+			
+		end if;
+	end process;
+
+dp2:	process(ram_addr_tmp)
+	begin
+		if reset = '0' and enable = '1' and draw_can_access = '1' then -- RAM is free to access
+			ramaddr <= ram_addr_tmp ;
+			ramdata <= ramdata_tmp;
+		else
+			ramaddr <= (others => 'Z');
+			ramdata <= (others => 'Z');
 		end if;
 	end process;
 end behaviour;
-
 
 
 
