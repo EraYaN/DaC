181e3e83ded977289d49f527869324d239b17c83
Robin Hes <ninjatuna@outlook.com>
2013-12-02 08:55:39 +0100

    agenda en draw_line in draw

--- hardware/gpu/draw-structural.vhd
+++ hardware/gpu/draw-structural.vhd
@@ -55,24 +55,24 @@ architecture structural of draw is
 	);
 	end component;
 	
-	-- component draw_line
-	-- port(
-		-- clk   : in    std_logic;
-		-- reset : in    std_logic;
-		-- enable: in    std_logic;
-		-- x  : in    std_logic_vector(SizeX-1 downto 0);
-		-- y  : in    std_logic_vector(SizeY-1 downto 0);
-		-- w  : in    std_logic_vector(SizeX-1 downto 0);
-		-- h  : in    std_logic_vector(SizeY-1 downto 0);
-		-- color : in std_logic_vector(SizeColor-1 downto 0);
-		-- asb : in std_logic;
-		-- done  : out   std_logic;
-		-- ramaddr     :out   std_logic_vector(SizeRAMAddr-1 downto 0);
-		-- ramdata     :out   std_logic_vector(SizeRAMData-1 downto 0);
-		-- draw_write :out std_logic;
-		-- draw_can_access : in std_logic
-	-- );
-	-- end component;
+	component draw_line2
+	port(
+		clk   : in    std_logic;
+		reset : in    std_logic;
+		enable: in    std_logic;
+		x0  : in    std_logic_vector(SizeX-1 downto 0);
+		y0  : in    std_logic_vector(SizeY-1 downto 0);
+		x1  : in    std_logic_vector(SizeX-1 downto 0);
+		y1  : in    std_logic_vector(SizeY-1 downto 0);
+		color : in std_logic_vector(SizeColor-1 downto 0);
+		asb : in std_logic;
+		done  : out   std_logic;
+		ramaddr     :out   std_logic_vector(SizeRAMAddr-1 downto 0);
+		ramdata     :out   std_logic_vector(SizeRAMData-1 downto 0);
+		draw_write :out std_logic;
+		draw_can_access : in std_logic
+	);
+	end component;
 
 	signal pixel_done,fill_done,rect_done,line_done : std_logic;
 	signal pixel_write,fill_write,rect_write,line_write : std_logic;
