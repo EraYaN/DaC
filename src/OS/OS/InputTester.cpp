7d2a28b9390ed6fdf52116f06621d66a83ab08b6
Erwin de Haan <git@erayan.eu>
2014-01-06 10:21:34 +0100

    Input Tester added

---
+++ src/OS/OS/InputTester.cpp
@@ -0,0 +1,19 @@
+#include "Util.h"
+void Demo::tick(unsigned long ctime){
+	GPU->cleanUp();	
+	GPU->drawFill(B000000);
+	for(int i =0; i < 2; i++){
+		if(
+	}
+	count++;
+	GPU->switchScreenBuffer();			
+}
+void Demo::start(){
+	
+}
+void Demo::stop(){
+	
+}
+void Demo::reset(){
+	
+}
